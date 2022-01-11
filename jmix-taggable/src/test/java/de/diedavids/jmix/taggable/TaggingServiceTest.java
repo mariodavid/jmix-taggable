0c494716c0db27afa6e2cbd6459a04fa828f3936
mariodavid <mario@die-davids.de>
2022-01-11 05:43:54 +0100

    add more tests

--- jmix-taggable/src/test/java/de/diedavids/jmix/taggable/TaggingServiceTest.java
+++ jmix-taggable/src/test/java/de/diedavids/jmix/taggable/TaggingServiceTest.java
@@ -12,9 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,7 +30,8 @@ class TaggingServiceTest {
     Tag small;
     Tag tall;
     Tag cool;
-    private Product taggable;
+    private Product iPhone;
+    private Product iPad;
 
     @BeforeEach
     void setUp() {
@@ -39,12 +39,13 @@ class TaggingServiceTest {
         tall = tag("tall");
         cool = tag("cool");
 
-        taggable = createTaggable();
+        iPhone = createProduct();
+        iPad = createProduct();
     }
 
     @Test
     void tagsCanBeAppliedToTaggableInstances() {
-        Product taggableProduct = createTaggable();
+        Product taggableProduct = createProduct();
 
         tagging(taggableProduct, small);
 
@@ -53,7 +54,7 @@ class TaggingServiceTest {
         assertEquals(taggableProduct, loadedProduct);
     }
 
-    private Product createTaggable() {
+    private Product createProduct() {
         Product taggableProduct = dataManager.create(Product.class);
         taggableProduct.setName("Product with Tags");
 
@@ -65,15 +66,103 @@ class TaggingServiceTest {
     class SetTagsForEntity {
 
         @Test
-        void tagsAnEntity_withOnlyOneTag_passedAsAParameter() {
+        void given_oneTag_expect_oneTag() {
 
-            //when:
-            sut.setTagsForEntity(taggable, asList(cool));
+            // when:
+            sut.setTagsForEntity(iPhone, asList(cool));
 
-            //then:
-            assertThat(sut.getTags(taggable))
+            // then:
+            assertThat(sut.getTags(iPhone))
                     .hasSize(1)
                     .containsOnly(cool);
+        }
+        @Test
+        void given_noTags_expect_noTags() {
+
+            // when:
+            sut.setTagsForEntity(iPhone, emptyList());
+
+            // then:
+            assertThat(sut.getTags(iPhone))
+                    .isEmpty();
+        }
+
+    }
+
+
+    @Nested
+    class SetTagsForEntityWithContext {
+
+        @Test
+        void given_oneTagWithContext_expect_oneTagForContext() {
+
+            // when:
+            sut.setTagsForEntityWithContext(iPhone, asList(cool), "context-1");
+
+            // then:
+            assertThat(sut.getTagsWithContext(iPhone, "context-1"))
+                    .hasSize(1)
+                    .containsOnly(cool);
+        }
+
+        @Test
+        void given_noTags_expect_noTags() {
+            // given:
+            final String aContext = "context-1";
+            final String aDifferentContext = "other-context";
+
+
+            // when:
+            sut.setTagsForEntityWithContext(iPhone, asList(cool), aContext);
+
+            // then:
+            assertThat(sut.getTagsWithContext(iPhone, aDifferentContext))
+                    .isEmpty();
+        }
+
+    }
+
+
+    @Nested
+    class GetEntitiesWithTag {
+
+        @Test
+        void given_iPhoneWithTagCool_and_iPadWithOtherTag_expect_resultIsIPhone() {
+
+            // given:
+            sut.setTagsForEntity(iPhone, asList(cool));
+            sut.setTagsForEntity(iPad, asList(tall));
+
+            // expect:
+            assertThat(sut.getEntitiesWithTag(cool))
+                    .hasSize(1)
+                    .containsOnly(iPhone);
+        }
+
+        @Test
+        void given_multipleProductsWithTag_expect_allProductsAreReturned() {
+
+            // given:
+            sut.setTagsForEntity(iPhone, asList(cool));
+            sut.setTagsForEntity(iPad, asList(cool));
+
+            // expect:
+            assertThat(sut.getEntitiesWithTag(cool))
+                    .hasSize(2)
+                    .containsOnly(iPad, iPhone);
+        }
+
+
+        @Test
+        void given_noProductWithTag_expect_emptyResult() {
+
+            // given:
+            sut.setTagsForEntity(iPhone, asList(cool));
+            sut.setTagsForEntity(iPad, asList(tall));
+
+            // expect:
+            assertThat(sut.getEntitiesWithTag(small))
+                    .isEmpty();
         }
 
     }
